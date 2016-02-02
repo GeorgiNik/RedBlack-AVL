b2b429d199b15d52e1820ada96fbed1501a3d95f
GeorgiNik <georgi.nik95@gmail.com>
2016-02-02 10:21:52 +0200

    architecture added

---
+++ RedBlackAvl/RedBlackAvl.Common/GlobalConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackAvl.Common
+{
+    public static class GlobalConstants
+    {
+    }
+}
