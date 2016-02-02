86eab255fe52d672d1a37de0f40db1513302d822
GeorgiNik <georgi.nik95@gmail.com>
2016-02-02 10:33:41 +0200

    avlNode added

---
+++ RedBlackAvl/RedBlackAvl.Implementation/Avl/AvlTree.cs
@@ -0,0 +1,34 @@
+﻿namespace RedBlackAvl.Implementation.Avl
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class AvlTree<TKey, TValue> : IEnumerable<TValue>
+    {
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class AvlNode
+        {
+            public AvlNode Parent { get; set; }
+
+            public AvlNode Left { get; set; }
+
+            public AvlNode Right { get; set; }
+
+            public TKey Key { get; set; }
+
+            public TValue Value { get; set; }
+
+            public int Balance { get; set; }
+        }
+    }
+}
