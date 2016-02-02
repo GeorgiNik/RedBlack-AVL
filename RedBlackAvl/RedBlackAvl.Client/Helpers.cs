c1274e8e3177f4d983cf1b7f06dfc86503777163
GeorgiNik <georgi.nik95@gmail.com>
2016-02-02 13:44:41 +0200

    console helpers class introduced

---
+++ RedBlackAvl/RedBlackAvl.Client/Helpers.cs
@@ -0,0 +1,19 @@
+﻿namespace RedBlackAvl.Client
+{
+    using System;
+    
+
+    public static class Helpers
+    {
+        public static void Traversing()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Traversing tree..");
+        }
+        public static void Removing(object removeItem)
+        {
+            Console.WriteLine("Removing {0}",removeItem);
+        }
+
+    }
+}
